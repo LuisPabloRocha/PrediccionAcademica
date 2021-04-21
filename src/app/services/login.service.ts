f8fb3affa457e4e53d6590922432cfe1fd02d4e4
raulbau <mrrbau@gmail.com>
2021-04-20 23:21:39 -0500

    se agregaron funciones para inicar y cerrar sesión

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,58 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private url: string;
 
-  constructor() { }
+  constructor(private _http: HttpClient) {
+    this.url = environment.url;
+  }
+
+  /**
+ * Servicio para iniciar sesión
+ * @param data Datos necesarios para iniciar sesión
+ * @returns "token"
+ */
+  login(data): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+    return this._http.post(this.url + 'auth/login', data, { headers: headers }).pipe(map(res => res));
+  }
+
+  /**
+   * Servicio para cerrar sesión
+   * @param token Token del usuario logeado
+   * @returns 
+   */
+  logOut(token): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': token
+    })
+    return this._http.post(this.url + 'auth/logout', { headers: headers }).pipe(map(res => res));
+  }
+
+  /**
+  * Función para obtener el token del usuario del sessionStorage
+  * @returns "token"
+  */
+  getToken() {
+    //Obtenemos el token del usuario del sessionStorage
+    return JSON.parse(sessionStorage.getItem('token'))
+  }
+  /**
+  * Función para guardar el token del usuario en el sessionStorage
+  * @param token Token del usuario
+  */
+  setToken(token) {
+    //Guardamos el token del usuario en el sessionStorage
+    sessionStorage.setItem('token', JSON.stringify(token));
+  }
 }
