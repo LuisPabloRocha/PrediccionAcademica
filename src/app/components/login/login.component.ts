26c8a94a2177479e1e22c0576e5b3579ae0ceca1
raulbau <mrrbau@gmail.com>
2021-04-21 00:30:19 -0500

    make it better

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  //#region Variables
+  /**Variable con los datos del usuario */
+  public user = {
+    /**Nombre de usuario */
+    "username": "",
+    /**Nombre */
+    "first_name": "",
+    /**Apellido */
+    "last_name": "",
+    /**Contraseña */
+    "password": ""
+  }
+  //#endregion
+  constructor(
+    private loginService: LoginService
+  ) { }
 
   ngOnInit(): void {
+    //Inicializamos los datos del usuario
+    this.inicializaUsuario()
+  }
+
+  /**
+   * Función para inicializar
+   */
+  inicializaUsuario() {
+    this.user = {
+      /**Nombre de usuario */
+      "username": "admin",
+      /**Nombre */
+      "first_name": "",
+      /**Apellido */
+      "last_name": "",
+      /**Contraseña */
+      "password": "nachoraul123"
+    }
+  }
+
+  /**
+   * Función para iniciar sesión
+   */
+  iniciarSesion() {
+    this.user.first_name = undefined
+    this.user.last_name = undefined
+    if (this.user.username != "" && this.user.password != "") {
+      //Iniciamos sesión
+      this.loginService.login(this.user).subscribe(
+        response => {
+          console.log(response);
+        },
+        error => {
+          console.log(error);
+          this.user.password = ""
+        }
+      )
+    }
   }
 
 }
