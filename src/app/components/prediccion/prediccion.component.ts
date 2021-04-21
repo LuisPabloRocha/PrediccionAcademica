8afef57b929d92d5e6cb66d2c12f7f8418574509
raulbau <mrrbau@gmail.com>
2021-04-21 11:21:49 -0500

    heroku

--- src/app/components/prediccion/prediccion.component.ts
+++ src/app/components/prediccion/prediccion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
+import { PredictionService } from 'src/app/services/prediction.service';
 
 @Component({
   selector: 'app-prediccion',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prediccion.component.css']
 })
 export class PrediccionComponent implements OnInit {
-
-  constructor() { }
+  public columns = []
+  public prediction;
+  constructor(
+    private predictionService: PredictionService,
+    private loginService: LoginService
+  ) { }
 
   ngOnInit(): void {
+    this.obtenColumnas()
+  }
+
+  obtenColumnas() {
+    this.predictionService.getColumns(this.loginService.getToken()).subscribe(
+      response => {
+        console.log(response);
+        this.columns = response
+        for (let i = 0; i < this.columns.length; i++) {
+          this.columns[i]['data'] = ""
+        }
+      },
+      error => {
+        console.log(error);
+
+      }
+    )
+  }
+
+  obtenPrediccion() {
+    this.prediction = null
+    let data = {}
+    this.columns.forEach(element => {
+      if (element.name != "tipoED1" && element.name != "tipoPen") {
+        data[element.name] = Number.parseInt(element.data)
+      } else {
+        data[element.name] = element.data
+      }
+    });
+    console.log(data)
+    this.predictionService.predict(data, this.loginService.getToken()).subscribe(
+      response => {
+        console.log(response)
+        if (response.answer!=null) {
+          this.prediction = response.answer[0]
+        }
+        console.log(this.prediction)
+      },
+      error => { console.log(error) }
+    )
   }
 
 }
---
+++ src/app/services/prediction.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PredictionService {
+  private url: string;
+
+  constructor(private _http: HttpClient) {
+    this.url = environment.url;
+  }
+
+  getColumns(token): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Token ' + token
+    })
+    return this._http.get(this.url + 'prediction/columns', { headers: headers }).pipe(map(res => res));
+  }
+  predict(data, token): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Token ' + token
+    })
+    return this._http.post(this.url + 'prediction/predict', data, { headers: headers }).pipe(map(res => res));
+  }
+}
