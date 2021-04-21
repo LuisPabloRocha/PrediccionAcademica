479d3b2fdb5076f4dab8bbc22e3e11c5c9524eff
raulbau <mrrbau@gmail.com>
2021-04-21 02:10:52 -0500

    Modificaciones para heroku

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
+// Serve static files
 app.use(express.static(__dirname + '/dist/prediccion-academica'));
-app.get('/', function (req, res) {
+
+// Send all requests to index.html
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/prediccion-academica/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
