14efeb09b357d329998c2f9772d8d689cfa63906
PabloRocha <luispablorocha@alumnos.uaslp.edu.mx>
2021-04-27 18:10:20 -0500

    Arreglo header login

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Input() tipo: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.tipo);
+    this.tipo = this.tipo;
   }
 
 }
