c4af1634b5c3ee5c0bf6f9c80ea7bce5e6a83288
raulbau <mrrbau@gmail.com>
2021-05-21 14:27:30 -0500

    se agregaron las nuevas peticiones

--- src/app/services/prediction.service.ts
+++ src/app/services/prediction.service.ts
@@ -21,12 +21,40 @@ export class PredictionService {
     })
     return this._http.get(this.url + 'prediction/columns', { headers: headers }).pipe(map(res => res)); //get -> post , columns -> fit 
   }
+  getModel(token): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Token ' + token
+    })
+    return this._http.get(this.url + 'prediction/model', { headers: headers }).pipe(map(res => res)); //get -> post , columns -> fit 
+  }
+  getModelPK(token): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Token ' + token
+    })
+    return this._http.get(this.url + 'prediction/model/pk', { headers: headers }).pipe(map(res => res)); //get -> post , columns -> fit 
+  }
+  modelPKFit(data, token): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Token ' + token
+    })
+    return this._http.post(this.url + 'prediction/model/pk/fit', data, { headers: headers }).pipe(map(res => res)); //get -> post , columns -> fit 
+  }
   predict(data, token): Observable<any> {
     let headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': 'Token ' + token
     })
     return this._http.post(this.url + 'prediction/predict', data, { headers: headers }).pipe(map(res => res));
+  }
+  predictPK(data, token): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Token ' + token
+    })
+    return this._http.post(this.url + 'prediction/predict/pk', data, { headers: headers }).pipe(map(res => res));
   }
   fit(token): Observable<any> {
     let headers = new HttpHeaders({
