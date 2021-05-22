587865aa3421af11f367931fe6542126de9b9018
PabloRocha <luispablorocha@alumnos.uaslp.edu.mx>
2021-05-22 14:55:30 -0500

    Prediccion alta, media, baja

--- src/app/components/prediccion/prediccion.component.ts
+++ src/app/components/prediccion/prediccion.component.ts
@@ -14,12 +14,14 @@ import { Label } from 'ng2-charts';
 })
 export class PrediccionComponent implements OnInit {
   public columns = []
+  public results = [] 
   public prediction;
   public widthANN: number;
   public widthNaive: number;
   public widthRF: number;
   public widthSVM: number;
   public promedio: number;
+  public resPositivo:number=0;
 
   constructor(
     private predictionService: PredictionService,
@@ -49,6 +51,7 @@ export class PrediccionComponent implements OnInit {
 
   obtenPrediccion() {
     this.prediction = null
+    this.resPositivo=0;
     let data = {}
     this.columns.forEach(element => {
       if (element.name != "tipoED1" && element.name != "tipoPen") {
@@ -77,8 +80,24 @@ export class PrediccionComponent implements OnInit {
     this.widthANN = this.prediction.ANN.SI * 100;
     this.widthNaive = this.prediction.NaiveBayes.SI * 100;
     this.widthSVM = this.prediction.SVM.SI * 100;
+    this.results.push(this.widthRF, this.widthANN, this.widthNaive, this.widthSVM);
+    this.obtienePosibilidad();
     let suma = this.widthRF + this.widthANN + this.widthNaive + this.widthSVM;
     this.promedio = Math.round(suma / 4);
+  }
+
+  obtienePosibilidad(){
+    console.log(this.results);
+    this.results.forEach(element => {
+      if(element >= 70){
+        this.resPositivo+=1;
+      }
+    });
+    if(this.prediction.Tree.SI==1){
+      this.resPositivo+=1;
+    }
+    console.log(this.resPositivo)
+    this.results = [];
   }
 
 }
