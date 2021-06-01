ea909079c75d244d1540e5017d6390ea105b8588
raulbau <mrrbau@gmail.com>
2021-06-01 13:31:29 -0500

    Se convierten los datos antes de la peticion

--- src/app/components/modelo/modelo.component.ts
+++ src/app/components/modelo/modelo.component.ts
@@ -14,13 +14,13 @@ export class ModeloComponent implements OnInit {
   @Input() modelo;
   @Output() actualizar = new EventEmitter();
   closeResult: string;
-  @ViewChild('content') myModal : any;
+  @ViewChild('content') myModal: any;
 
 
   constructor(
     private predictionService: PredictionService,
     private loginService: LoginService,
-    private spinnerService : SpinnerService,
+    private spinnerService: SpinnerService,
     private modalService: NgbModal
   ) {
 
@@ -34,6 +34,40 @@ export class ModeloComponent implements OnInit {
     //console.log(this.modelo);
     let data = { attributes: [] }
     this.modelo.attributes.forEach(element => {
+      // console.log(element);
+      let s = ""
+      s.replace("<class \'", "")
+      let type = element.dataType.replace("<class \'", "").replace("'>", "")
+      // console.log(type);
+      switch (type) {
+        case "tuple":
+
+          break;
+        case "str":
+          element.actualValue = element.actualValue.toString()
+          break;
+        case "float":
+          element.actualValue = Number.parseFloat(element.actualValue)
+          break;
+        case "int":
+          element.actualValue = Number.parseInt(element.actualValue);
+          break;
+        case "bool":
+          switch (element.actualValue.toString().toLowerCase()) {
+            case "true":
+              element.actualValue = true
+              break;
+            case "false":
+              element.actualValue = false
+              break;
+            default:
+              element.actualValue = false
+              break;
+          }
+        default:
+          break;
+      }
+
       data.attributes.push({ name: element.name, value: element.actualValue })
     });
     console.log(data);
@@ -43,11 +77,10 @@ export class ModeloComponent implements OnInit {
         console.log(response);
         this.actualizar.emit(true)
         this.open(this.myModal)
-        setTimeout(() => 
-        {
-            this.modalService.dismissAll()
+        setTimeout(() => {
+          this.modalService.dismissAll()
         },
-        7000);
+          7000);
       },
       error => {
         console.log(error);
@@ -55,7 +88,7 @@ export class ModeloComponent implements OnInit {
     )
   }
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', windowClass: 'custom-class', centered: true, animation:true, size: 'sm'},  ).result.then((result) => {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', windowClass: 'custom-class', centered: true, animation: true, size: 'sm' },).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
