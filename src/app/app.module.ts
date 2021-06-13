d1d350df6b1b74f6c862a1de1615a880bcf2ebc2
raulbau <mrrbau@gmail.com>
2021-06-13 02:10:25 -0500

    Se eliminaron componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { AlumnosComponent } from './components/alumnos/alumnos.component';
 import { LoginComponent } from './components/login/login.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { AlgoritmoComponent } from './components/algoritmo/algoritmo.component';
 import { PrediccionComponent } from './components/prediccion/prediccion.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
-import { BarchartComponent } from './components/barchart/barchart.component';
 import { CircleprogressComponent } from './components/circleprogress/circleprogress.component';
 import {RoundProgressModule} from 'angular-svg-round-progressbar';
 import { ProfesoresComponent } from './components/profesores/profesores.component';
@@ -32,15 +29,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
     HeaderComponent,
     HomeComponent,
     AlumnosComponent,
     LoginComponent,
-    FooterComponent,
     AlgoritmoComponent,
     PrediccionComponent,
-    BarchartComponent,
     CircleprogressComponent,
     ProfesoresComponent,
     NavsidebarComponent,
